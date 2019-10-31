cd9da5ae84ab95c0fab6508c082216a7ae96f1bd
denshade <lieven.vaneeckhaute@gmail.com>
2019-10-31 22:55:06 +0100

    first draft.

--- loadDns.py
+++ loadDns.py
@@ -1,2 +1,16 @@
-f = open("guru99.txt","w+")
-    
+with open("hosts.nonencrypted") as f:
+  lineList = f.readlines()
+
+with open('c:\windows\System32\drivers\etc\hosts') as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+        print(line, end = '')
+        line = fp.readline()
+        cnt += 1
+        if line == "## DON'T EDIT AFTER THIS LINE":
+            break
+print("## DON'T EDIT AFTER THIS LINE")
+with open("hosts.nonencrypted") as f:
+    line = f.readline()
+    print(line, end='')
